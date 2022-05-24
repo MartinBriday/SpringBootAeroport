eb735cbcc864d6437f32ce70b08d1b3a5694ffac
Melissa-intiformation <melissa.z2101@gmail.com>
2022-05-24 11:43:12 +0200

    Ajout des droits pour les nouveaux liens

--- src/main/java/com/intiformation/SpringBootAeroport/configuration/SpringSecurityConfiguration.java
+++ src/main/java/com/intiformation/SpringBootAeroport/configuration/SpringSecurityConfiguration.java
@@ -33,7 +33,7 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter
 	        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 	        .and()
 			.authorizeRequests()
-			.antMatchers("/login", "/register_utilisateur_form", "/init", "/accueil").permitAll()
+			.antMatchers("/login", "/register_utilisateur_form", "/init", "/accueil", "/affichageVols", "/vol_choisi", "/infos_client_passager", "/recupInfo").permitAll()
 			.antMatchers("/admin/**").hasRole("ADMIN")
 //			.antMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
 			.anyRequest().authenticated()
